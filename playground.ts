c4d3d5c65231c2e490c943880eaf1a9c20afef55
arshad <arshadpyaseen@gmail.com>
2025-10-17 18:14:48 +0530

    chore: update

--- playground.ts
+++ playground.ts
@@ -1,10 +1,5 @@
 import { generateDts } from './src'
 
 console.time('time')
-const result = await generateDts(['project/index.ts'], {
-	inferTypes: true,
-	tsgo: true,
-})
+await generateDts(['src/index.ts'])
 console.timeEnd('time')
-
-console.log(result.files[0].dts)
