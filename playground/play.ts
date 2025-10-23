63fa2d356ff2070347120a9fca28354781f85a06
arshad <arshadpyaseen@gmail.com>
2025-10-24 00:56:17 +0530

    chore: update

--- playground/play.ts
+++ playground/play.ts
@@ -3,6 +3,7 @@ import { generateDts } from '../src'
 console.time('time')
 const result = await generateDts(['playground/src/index.ts'], {
 	inferTypes: true,
+	minify: true,
 })
 for (const file of result.files) {
 	await Bun.write(`playground/dist/${file.outputPath}`, file.dts)
